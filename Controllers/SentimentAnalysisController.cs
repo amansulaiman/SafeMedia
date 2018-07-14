dfd5acf837b3373c1b0bd42ba150b97876275a0f
Abdulrahman Sulaiman <amansulaiman92@gmail.com>
2018-07-14 08:46:37 +0100

    Model training and some data changes

--- Controllers/SentimentAnalysisController.cs
+++ Controllers/SentimentAnalysisController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using hateSpeach.Models;
 using hateSpeach.Services;
@@ -26,10 +29,70 @@ namespace hateSpeach.Controllers
             {
                 var predictedLanguage = await MLPrediction.PredictLanguageAsync(new LanguageModel() { SentimentText = sentiment });
                 var predictedSentiment = await MLPrediction.PredictSentimentAsync(new SentimentModel() { SentimentText = sentiment });
-
-                return Ok("");
+                float LanguageConfidance = 0.0f;
+                if (predictedLanguage.PredictedLabels == "HAUSA")
+                {
+                    LanguageConfidance = predictedLanguage.Score[0] * 100;
+                }
+                if (predictedLanguage.PredictedLabels == "ENGLISH")
+                {
+                    LanguageConfidance = predictedLanguage.Score[1] * 100;
+                }
+                float hateSpeachConfidance = 0.0f;
+                if (predictedSentiment.PredictedLabels)
+                {
+                    hateSpeachConfidance = predictedSentiment.Score * 100;
+                }else
+                {
+                    hateSpeachConfidance = 0;
+                }
+                string suggestion = string.Empty;
+                if (predictedLanguage.PredictedLabels == "HAUSA" && predictedSentiment.PredictedLabels)
+                {
+                    suggestion = "Ka dan gyara kalamanka domin za su iya cutar da wani";
+                }
+                if (predictedLanguage.PredictedLabels == "ENGLISH" && predictedSentiment.PredictedLabels)
+                {
+                    suggestion = "Please refine your world they might be offensive to others";
+                }
+                return Ok(new SentimentViewModel(){
+                    Language = predictedLanguage.PredictedLabels, LanguageConfidance = LanguageConfidance, IsHateSpeech = predictedSentiment.PredictedLabels, HateSpeechConfidance = hateSpeachConfidance, Suggestion = suggestion 
+                });
             }
             return NotFound("Invalid text");
+        }
+
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        
+        [HttpGet("[action]")]
+        public IEnumerable<WeatherForecast> WeatherForecasts()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+        }
+
+        public class WeatherForecast
+        {
+            public string DateFormatted { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+
+            public int TemperatureF
+            {
+                get
+                {
+                    return 32 + (int)(TemperatureC / 0.5556);
+                }
+            }
         }
     }
 }
